2c79b426f02769e8203dd1ec1fb4bd75853737db
BougtabMohammed <bougtab.mohammed03@gmail.com>
2025-07-29 17:03:16 +0100

    CLean full structured project

---
+++ source code/backend-spring boot/src/main/java/com/zosh/domain/USER_ROLE.java
@@ -0,0 +1,12 @@
+package com.zosh.domain;
+
+public enum USER_ROLE {
+	
+// les trois types de role pour Un User
+	    ROLE_CUSTOMER,
+	    ROLE_SELLER,
+	    ROLE_ADMIN
+	
+
+
+}
---
+++ source code/backend-spring boot/src/main/java/com/zosh/model/User.java
@@ -0,0 +1,49 @@
+package com.zosh.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zosh.domain.USER_ROLE;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+    //pour generer id directement
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // La permition de Ecrire Seulement le mot de passe et non de Lire
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    // @Column(nullable = false) c-a-d que la colonne ne peut pas contenir de valeur NULL
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    private String mobile;
+
+    private USER_ROLE role;
+
+    // 1 User peut avoir 1 ou plusieurs Address
+    @OneToMany
+    private Set<Address> addresses=new HashSet<>();
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_coupons",
+            inverseJoinColumns = @JoinColumn(name = "coupon_id")
+    )
+    private Set<Coupon> usedCoupons=new HashSet<>();
+
+}
